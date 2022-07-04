36b19dff6f7f2d75053a07f749d9b92a5187374c
quyt0 <dphuc2304@gmail.com>
2022-07-04 16:11:28 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,37 @@
-$('.owl-carousel').owlCarousel({
-    items: 1,
-    loop: true,
-    autoheight: true,
-    nav: true,
-    autoplay: true,
-    autoplayTimeout: ("15" * 1000) || 7000,
-})
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector("header").style.display = "block";
+  } else {
+     document.querySelector("header").style.display = "none";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+$(document).ready(function() {
+    $('.owl-carousel.banner').owlCarousel({
+        items: 1,
+        loop: true,
+        autoheight: true,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: ("15" * 1000) || 7000,
+    });
+
+    $('.owl-carousel.store').owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,
+        autoheight: true,
+        autoplayTimeout: ("15" * 1000) || 7000,
+    });
+
+    $('.store__img-slider').owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,
+        autoheight: true,
+        autoplayTimeout: ("15" * 1000) || 7000,
+    });
+});
